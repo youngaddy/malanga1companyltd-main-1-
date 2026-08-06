7e322948abda5df0a88152554024287f5746e9d6
youngaddy <mutalaadnan4@gmail.com>
2026-08-06 00:37:24 +0000

    Use paid Render plans with persistent disk for media uploads

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -155,7 +155,7 @@ STORAGES = {
 }
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = Path(os.environ.get('DJANGO_MEDIA_ROOT', str(BASE_DIR / 'media')))
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
